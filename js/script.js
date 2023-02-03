faa1560a9ffdb9cf1a4cb70399aa2f2e18327aa5
Klisman Mateus <klismanmts@gmail.com>
2023-02-02 23:05:12 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+function openInNewTab1() {
+    var url1 = ""
+    var btn1 = document.getElementById("btn1")
+
+    window.open(url1)
+    
+}   
+
+function openInNewTab2() {
+    var url2 = ""
+    var btn2 = document.getElementById("btn2")
+
+    window.open(url2)
+    
+}  
+
+function openInNewTab3() {
+    var url3 = ""
+    var btn3 = document.getElementById("btn3")
+
+    window.open(url3)
+    
+}  
